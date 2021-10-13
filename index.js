61ab758cde2e923e057917d07054f4b91f78c28a
KatherineYuneman <kyuneman@gmail.com>
2021-10-13 15:53:48 -0400

    commit tree 7a83dc80c871be18b6364bff58662b589ee0d79d

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 function scuberGreetingForFeet(feet) {
   if (feet > 2500) {
     let message = "No can do.";
+    console.log(message);
     return message;
   } else if (feet > 2000) {
     let message = "I will gladly take your thirty bucks.";
+    console.log(message);
     return message;
   } else if (feet <= 400) {
     let message = "This one is on me!";
+    console.log(message);
     return message;
   } else message = "I will gladly take your fifteen bucks."
+  console.log(message);
+  return message;
+
 }
 scuberGreetingForFeet(2600);
-
-function ternaryCheckCity(city){
+/*
 const message = city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
 return message;
-}
-ternaryCheckCity('NYC');
 
 
 let tiptype;
@@ -32,3 +35,4 @@ function switchOnCharmFromTip(tiptype){
         return message="Bye.";
 }};
 switchOnCharmFromTip('generous')
+*/
