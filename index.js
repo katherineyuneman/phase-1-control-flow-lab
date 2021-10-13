d2005b62f31d4b350eeaa54f6971c3b3c392a01b
KatherineYuneman <kyuneman@gmail.com>
2021-10-13 15:13:55 -0400

    Completed assignment need to check 1 error'

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet > 2500) {
+    let message = "No can do.";
+    return message;
+  } else if (feet > 2000) {
+    let message = "I will gladly take your thirty bucks.";
+    return message;
+  } else if (feet <= 400) {
+    let message = "This one is on me!";
+    return message;
+  } else message = "I will gladly take your fifteen bucks."
 }
+scuberGreetingForFeet(2600);
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+const message = city = 'NYC' ? 'Ok, sounds good.' : 'No go.';
+return message;
 }
+ternaryCheckCity('NYC');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+let tiptype;
+function switchOnCharmFromTip(tiptype){
+  switch (tiptype) {
+    case 'generous':
+        let message = "Thank you so much.";
+        return message;
+    case 'not as generous':
+        message = "Thank you.";
+        return message;
+    default:
+        return message="Bye.";
+}};
+switchOnCharmFromTip('generous')
